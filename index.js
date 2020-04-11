e90773aebfd68d064a343800315b552d674a174c
Rachel Kat <rachelkat@gmail.com>
2020-04-11 02:17:26 +0000

    add index.js

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ function logTaskState(task) {
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
+const task3 = {};
+const task4 = {};
 
 task1.logState(); // Clean Cat Litter has not been completed
 task1.markCompleted();
